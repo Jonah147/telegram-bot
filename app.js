aafa011d74727c6bd29934940d332575255b3b5f
Jonah <yoniman16@gmail.com>
2018-06-17 01:26:29 +0300

    message

--- app.js
+++ app.js
@@ -17,26 +17,17 @@ var admin = require('firebase-admin');
 admin.initializeApp({
   credential: admin.credential.applicationDefault()
 });
-var db = firebase.database();
-var tasks = [];
-function getTasks() {
-  return db.ref('/tasks/').once('value').then(function(snapshot) {
-  tasks.push(snapshot.val());
-  console.log(snapshot.val());
-  });
-}
-function addTask(task,date) {
-  if(!date) {
-    date = new Date();
-    date = date.toString();
-  }
-  db.ref('/tasks/').set({
-    task: task,
-    date: date
-    });
-}
-getTasks();
-console.log(tasks);
+// var tasks = [];
+// function getTasks() {
+//   return db.ref('/tasks/').once('value').then(function(snapshot) {
+//   tasks.push(snapshot.val());
+//   console.log(snapshot.val());
+//   });
+// }
+const firestore = firebase.firestore();
+ const settings = {/* your settings... */ timestampsInSnapshots: true};
+ firestore.settings(settings);
+ var db = firebase.firestore();
 function addTask() {}
 var port = process.env.PORT || 8080;
 
@@ -56,19 +47,37 @@ bot.onText(/\/echo (.+)/, (msg, match) => {
   bot.sendMessage(chatId, resp);
 });
 addTask('holla',null);
-bot.on('message', (msg) => {
-  const chatId = msg.chat.id;
-  if(addingTask) {
-    addTask(msg.text.toString(),null);
-  } else if(msg.text !== '/add') {
-    bot.sendMessage(chatId, 'Hello sir use /add to add a task');
-  }
-});
+// bot.on('message', (msg) => {
+//   const chatId = msg.chat.id;
+//   if(addingTask) {
+//     addTask(msg.text.toString(),null);
+//   } else if(msg.text !== '/add') {
+//     bot.sendMessage(chatId, 'Hello sir use /add to add a task');
+//   }
+// });
+task = {
+  task: "karate",
+  done: false,
+  priority: "High",
+  notes: "do some kungfu...",
+  background: ""
+}
+
 bot.onText(/\/add/,(msg) => {
   bot.sendMessage(msg.chat.id, "add your task...");
+
+
+ //
+ db.collection('cities').doc('ef').set(task);
+
   addingTask = true;
 });
-console.log(db.ref('/tasks/'));
+bot.onText(/\/list/,(msg) => {
+  for(task of data) {
+    bot.sendMessage(msg.chat.id,"Hello man");
+  }
+  addingTask = true;
+});
 
 
 app.listen(port, function() {
